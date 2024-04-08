8daba2f3333a1648a2c2c60372fbee93921ee864
springhan <3379184879@qq.com>
2024-04-08 19:54:00 +0800

    Removed useless flutter components

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,12 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 
 import './game/game.dart';
-import './components/main_page.dart';
 
 void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // Flame.device.fullScreen();
-  // Flame.device.setPortrait();
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setPortrait();
 
-  // final game = TetrisGame();
-  runApp(const MainPage());
+  final game = TetrisGame();
+  runApp(GameWidget(game: game));
 }
