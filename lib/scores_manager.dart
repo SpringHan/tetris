b2cb120247db09685776953e73aa30574611fe76
springhan <3379184879@qq.com>
2024-04-08 19:55:49 +0800

    Only store the max score in any time

--- lib/scores_manager.dart
+++ lib/scores_manager.dart
@@ -10,20 +10,13 @@ Future<void> storeNewScore(int score) async {
   final file = File(scoreFile);
   String content;
 
-  if (!file.existsSync()) {
-    await file.create();
-    content = "$score";
-  } else {
-    content = await file.readAsString();
-    final scoreList = content.split("\n");
-    scoreList.sort((a, b) => b.toInt().compareTo(a.toInt()));
-    content = scoreList.join("\n");
-  }
-
+  if (!file.existsSync()) await file.create();
+  
+  content = "$score";
   await file.writeAsString(content);
 }
 
-Future<List<int>> fetchScores() async {
+Future<int> fetchScore() async {
   final cacheDirectory = await getExternalStorageDirectory();
   final scoreFile = "${cacheDirectory!.path}/scores.txt";
 
@@ -31,22 +24,10 @@ Future<List<int>> fetchScores() async {
 
   if (!file.existsSync()) {
     await file.create();
-    return [];
+    return 0;
   }
 
   final content = await file.readAsString();
-  final stringList = content.split("\n");
 
-  final List<int> scores = [];
-  for (final s in stringList) {
-    scores.add(s.toInt());
-  }
-
-  return scores;
-}
-
-extension ToInt on String {
-  int toInt() {
-    return int.parse(this);
-  }
+  return int.parse(content);
 }
